bfc58b3e9ea00124713824b0014a7badccca06f3
Olivia Madden <madden.livi@gmail.com>
2019-12-16 22:23:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["dog", "cat", "mouse", "horse"]
+  array = ["dog", "cat", "mouse", "horse"]
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << 
+  array << "pig" 
 end
 
 def add_element_to_start_of_array(array, element)
